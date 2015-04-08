1244f9f7134c62d8d7d06e9e086fef4b9ab9a02d
Blaine Bublitz <blaine@iceddev.com>
2015-04-08 15:55:58 -0700

    add watch for easier dev

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
+const watch = (process.argv.indexOf('--watch') !== -1);
+
 gulp.task('default', function(){
   return gulp.src('./src/*.js')
     .pipe(babel())
     .pipe(gulp.dest('.'));
 });
+
+if(watch){
+  gulp.watch('./src/*.js', gulp.parallel('default'));
+}
